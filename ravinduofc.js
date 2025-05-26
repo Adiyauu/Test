6435e34393c61bd5c6828eda81f1e1373507ddc4
RAVINDU_MD <Katharinabto@gmail.com>
2025-05-26 22:19:58 +0530

    Update ravinduofc.js

--- ravinduofc.js
+++ ravinduofc.js
@@ -117,7 +117,7 @@ async function downloadAndExtractZip() {
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=", '');
+const sessdata = config.SESSION_ID.replace("𝚁𝙰𝚅𝙸𝙽𝙳𝚄-𝙼𝙳=", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
